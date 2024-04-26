a2a8eeb725f8b08ffae9411d5025a3cf21c19ca5
AnErrupTion <anerruption@disroot.org>
2024-04-26 13:34:25 +0200

    Fix bug when VmFolder doesn't exist
    Signed-off-by: AnErrupTion <anerruption@disroot.org>

--- VirtualWorkstation/GlobalSettings.cs
+++ VirtualWorkstation/GlobalSettings.cs
@@ -47,7 +47,7 @@ public static class GlobalSettings
         {
             Directory.CreateDirectory(ConfigDirectory);
             _configModel = [];
-            VmFolder = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "VirtualWorkstation");
+            Directory.CreateDirectory(VmFolder = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "VirtualWorkstation"));
             CustomQemuPath = string.Empty;
             return;
         }
