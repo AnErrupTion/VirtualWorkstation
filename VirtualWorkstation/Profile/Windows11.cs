d4f91fdd64ce71d76366e28b8a551f0376f8398f
AnErrupTion <anerruption@disroot.org>
2024-04-27 15:07:53 +0200

    Add Windows 11 profile, make Windows profiles use VirtIO
    Signed-off-by: AnErrupTion <anerruption@disroot.org>

--- VirtualWorkstation/Profile/Windows11.cs
+++ VirtualWorkstation/Profile/Windows11.cs
@@ -5,17 +5,17 @@ namespace VirtualWorkstation.Profile;
 
 public static partial class Profiles
 {
-    private static readonly VirtualMachine Windows10 = new()
+    private static readonly VirtualMachine Windows11 = new()
     {
         Architecture = Architecture.Amd64,
         UseHardwareAcceleration = true,
-        Firmware = new Firmware { Type = FirmwareType.Efi },
+        Firmware = new Firmware { Type = FirmwareType.EfiSecureBoot },
         Chipset = new Chipset { Model = ChipsetModel.X86Q35, Q35Options = new Q35Options { EnablePs2Emulation = false, AcpiState = AcpiChipsetState.On } },
-        Ram = 2048,
+        Ram = 4096,
         Display = new Display { Type = DisplayType.Auto },
         AudioHostDevice = new AudioHostDevice { Type = AudioHostType.Auto },
         Processor = new Processor { Model = ProcessorModel.X86Host, Sockets = 1, Cores = 2, Threads = 1 },
-        TrustedPlatformModule = new TrustedPlatformModule { DeviceType = TpmDeviceType.None },
+        TrustedPlatformModule = new TrustedPlatformModule { DeviceType = TpmDeviceType.Emulated, Type = TpmType.Crb, Version = TpmVersion.V20 },
         NetworkInterfaces =
         [
             new NetworkInterface { Type = NetworkType.Nat, Card = NetworkCard.VirtIo }
